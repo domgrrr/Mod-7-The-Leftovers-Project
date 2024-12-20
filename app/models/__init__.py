9c3d6aa55e77a04be5e793e2c0ca30ee4dc8b785
Hannah Sheldon <hannahzsheldon@gmail.com>
2024-12-19 16:55:06 -0800

    added container seeder and tested migration

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,4 @@
 from .db import db
 from .user import User
+from .container import Container
 from .db import environment, SCHEMA
---
+++ app/seeds/containers.py
@@ -0,0 +1,26 @@
+from app.models import db, Container, environment, SCHEMA
+from sqlalchemy.sql import text
+
+def seed_containers():
+    for i in range(1, 4):
+        pantry = Container(
+            user_id=i, storage_type='pantry'
+        )
+        fridge = Container(
+            user_id=i, storage_type='fridge'
+        )
+        freezer = Container(
+            user_id=i, storage_type='freezer'
+        )
+        db.session.add(pantry)
+        db.session.add(fridge)
+        db.session.add(freezer)
+    db.session.commit()
+
+def undo_containers():
+    if environment == "production":
+        db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
+    else: 
+        db.session.execute(text("DELETE FROM containers"))
+        
+    db.session.commit()
