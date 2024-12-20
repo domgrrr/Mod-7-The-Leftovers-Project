a44f24d6d4898585c50190683c64996779487d4b
krystalg80 <krystalsoccer80@gmail.com>
2024-12-20 13:03:10 -0800

    created model and seed

---
+++ app/seeds/recipes.py
@@ -0,0 +1,23 @@
+from app.models import db, environment, SCHEMA, Recipe
+
+def seed_recipes():
+    demo_recipes = [
+        #just some example recipes that will be given to the user once loggied in / signed up
+        #setting the user id to 0 means that it is a default recipe that all users will have access to, so nothing specific to user
+        Recipe(user_id=0, name='Demo Recipe 1', directions='Mix ingredients...', image_url='http://example.com/image1.jpg', ingredients=["flour", "sugar", "butter"]),
+        Recipe(user_id=0, name='Demo Recipe 2', directions='Bake at 350 degrees...', image_url='http://example.com/image2.jpg', ingredients=["eggs", "milk", "vanilla"]),
+        Recipe(user_id=0, name='Demo Recipe 3', directions='Boil water...', image_url='http://example.com/image3.jpg', ingredients=["pasta", "salt", "olive oil"]),
+        # Add recipes before completing the seed
+    ]
+
+    for recipe in demo_recipes:
+        db.session.add(recipe)
+
+    db.session.commit()
+
+def undo_recipes():
+    if environment == "production":
+        db.session.execute(f'TRUNCATE table {SCHEMA}.recipes RESTART IDENTITY CASCADE;')
+    else:
+        db.session.execute('DELETE FROM recipes')
+    db.session.commit()
