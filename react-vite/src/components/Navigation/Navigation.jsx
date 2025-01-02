227854d8c0d5a74ae5f3555ede7a701a2f91319b
krystalg80 <krystalsoccer80@gmail.com>
2025-01-02 14:03:27 -0800

    recent push

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -8,7 +8,9 @@ function Navigation() {
       <li>
         <NavLink to="/">Home</NavLink>
       </li>
-
+      <li>
+        <NavLink to="/recipes">Recipes</NavLink>
+      </li>
       <li>
         <ProfileButton />
       </li>
