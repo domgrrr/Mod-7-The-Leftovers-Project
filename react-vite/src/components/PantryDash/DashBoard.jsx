40265f2bf458cdc27117a23756628f0d041b5c79
krystalg80 <krystalsoccer80@gmail.com>
2025-01-04 23:37:40 -0800

    added icons in dashboard

--- react-vite/src/components/PantryDash/DashBoard.jsx
+++ react-vite/src/components/PantryDash/DashBoard.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { getAllContainers } from "../../redux/container";
+import { FaBook, FaShoppingCart, FaWarehouse, FaSnowflake, FaBoxOpen } from 'react-icons/fa';
 import "./DashBoard.css";
 
 function DashBoard() {
@@ -20,13 +21,23 @@ function DashBoard() {
     return (
         <div className="dashboard-container">
             <div className="dashboard-links top-row">
-                <Link to="/recipes">Recipes</Link>
-                <Link to="/groceries">Groceries</Link>
+                <Link to="/recipes">
+                    <FaBook /> Recipes
+                </Link>
+                <Link to="/groceries">
+                <FaShoppingCart /> Groceries
+                </Link>
             </div>
             <div className="dashboard-links bottom-row">
-                <Link to={`/container/${containers?.containers[0].id}`}>Pantry</Link>
-                <Link to={`/container/${containers?.containers[1].id}`}>Fridge</Link>
-                <Link to={`/container/${containers?.containers[2].id}`}>Freezer</Link>
+                <Link to={`/container/${containers?.containers[0].id}`}>
+                <FaBoxOpen /> Pantry
+                </Link>
+                <Link to={`/container/${containers?.containers[1].id}`}>
+                <FaWarehouse /> Fridge
+                </Link>
+                <Link to={`/container/${containers?.containers[2].id}`}>
+                <FaSnowflake /> Freezer
+                </Link>
                 {/*<Link>All</Link>*/}
             </div>
         </div>
